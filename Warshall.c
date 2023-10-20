98198c2ea3ba60bf73c06c7e67e4c979c04a745c
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-20 16:48:09 +0530

    Create Warshall.c

---
+++ Warshall.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+
+void floydWarshall(int V, int graph[V][V]) {
+    int dist[V][V];
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print the shortest paths or update the 'dist' matrix as needed
+}
