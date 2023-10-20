6636689208c1f3d865123f3bfb78e3c3926235d2
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-20 16:48:24 +0530

    Create Warshall.java

---
+++ Warshall.java
@@ -0,0 +1,21 @@
+public static void floydWarshall(int V, int[][] graph) {
+    int[][] dist = new int[V][V];
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print the shortest paths or update the 'dist' matrix as needed
+}
